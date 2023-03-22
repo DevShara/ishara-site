d7aba97fc48343cfccbe4854fe2bb0b358ce2ddb
Devshara <developer.ishara@gmail.com>
2023-03-22 01:16:45 +0000

    the site header

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client";
 
 const App = () => {
   return (
-    <>
-      <h1>RENDERED</h1>
-    </>
+    <div className=" flex flex-col  items-center justify-center h-screen">
+      <h1 className=" text-3xl leading-snug  text-center">
+        <span className=" font-bold">ISHARA SAMUDITHA</span>
+        <br/>
+        IS
+        <br />
+        A WEB DEVELOPER
+      </h1>
+    </div>
   );
 };
 
